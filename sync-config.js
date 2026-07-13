2d243cad4019583c97e6bd45587957e15d710284
sharinskiy888-art <sharinskiy888@gmail.com>
2026-07-13 17:49:52 +0300

    Create sync-config.js

---
+++ sync-config.js
@@ -0,0 +1,6 @@
+// Публичные параметры Supabase. Anon-ключ предназначен для клиентских приложений;
+// доступ к данным дополнительно ограничивается политиками Row Level Security.
+window.SUPABASE_CONFIG = {
+  url: 'https://snhysfguxkgkcwwfgbkd.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNuaHlzZmd1eGtna2N3d2ZnYmtkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM5NDAyMTQsImV4cCI6MjA5OTUxNjIxNH0.KuXvyH2k5EN8qVNgJ_xUMsS1_MLN957IP-0wPxXpV8s'
+};
